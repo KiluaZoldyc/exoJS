8dd51a6bd1a12698d01d04b871c2b0516165ce41
Fred GUIRAUD <kiluaws@gmail.com>
2017-03-09 15:09:06 +0100

    exo fini

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,38 @@
 //REVISIONS
 // Declarez une variable nommée "boucler" contenant true
-
+var boucler = true
 
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
-
+var members = ["Aida67","lapie002","anneserrano","Jennysmille","nunkabuk","RCosson","kaonb-ax","FerEmilie","crazychouwi","KiluaZoldyc","patatobeur","Sam11360","elo062","hermeline","Biciclet"]
 // SI la variable boucler vaut true ALORS
+var check=document.getElementById("check");
+check.addEventListener("click",function(){
+if (boucler = true){
 
   // Bouclez sur le tableau que vous avez déclaré ci-dessus
+for (var i = 0 ; i < members.length; i++ ){
 
   // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo des autres
+switch (members[i]){
+case "KiluaZoldyc":
+var ecran=document.createElement("div");
+var p=document.getElementById("check");
+ecran.innerHTML="Affiche"+" "+"KiluaZoldyc";
+p.appendChild(ecran);
+console.log("Affiche"+" "+"KiluaZoldyc");
+break;
+default:
+var ecran=document.createElement("div");
+var p=document.getElementById("check");
+ecran.innerHTML=members[i];
+p.appendChild(ecran);
+
+
+console.log(members[i]);
+}
+}
+}	
+})
 
 // FIN REVISIONS
 
